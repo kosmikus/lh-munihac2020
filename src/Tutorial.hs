8866f18c224881d370e522c40b9393f42c8c94e8
Andres Loeh <andres@well-typed.com>
2020-09-11 18:43:48 +0200

    Final version.

--- src/Tutorial.hs
+++ src/Tutorial.hs
@@ -48,6 +48,76 @@ weekday = Sa
 impossible :: String -> a
 impossible message = error message
 
-{-@ x :: { xs : [ { i : Int | i == 2 } ] | len xs == 5 } @-}
-x :: [Int]
-x = replicate 5 2
+insert :: Ord a => a -> [a] -> [a]
+insert x []       = [x]
+insert x (y : ys)
+  | x <= y        = x : y : ys
+  | otherwise     = y : insert x ys
+
+isort :: Ord a => [a] -> [a]
+isort = foldr insert []
+
+data SortedList a = Nil | a :<= SortedList a
+infixr 5 :<=
+
+{-@ data SortedList a = Nil | (:<=) { hd :: a, tl :: SortedList { x : a | hd <= x } } @-}
+
+exampleSorted :: SortedList Int
+exampleSorted = 2 :<= 3 :<= 4 :<= Nil
+
+insert' :: Ord a => a -> SortedList a -> SortedList a
+insert' x Nil     = x :<= Nil
+insert' x (y :<= ys)
+  | x <= y        = x :<= y :<= ys
+  | otherwise     = y :<= insert' x ys
+
+{-@ insert'' :: forall < p :: Int -> Bool > . Int<p> -> SortedList (Int<p>) -> SortedList (Int<p>) @-}
+insert'' :: Int -> SortedList Int -> SortedList Int
+insert'' x Nil     = x :<= Nil
+insert'' x (y :<= ys)
+  | x <= y        = x :<= y :<= ys
+  | otherwise     = y :<= insert'' x ys
+
+{-@ assume plus :: x : Int -> y : Int -> { z : Int | z == x + y } @-}
+plus :: Int -> Int -> Int
+plus = (+)
+
+-- foldr :: (a -> r -> r) -> r -> [a] -> r
+
+isort' :: Ord a => [a] -> SortedList a
+isort' = foldr insert' Nil
+
+{-@ type SortedList' a = [a]<{\ x y -> x <= y }> @-}
+
+{-@ example' :: SortedList' Integer @-}
+example' :: [Integer]
+example' = [1,4,5]
+
+{-@ merge :: Ord a => xs : SortedList' a -> ys : SortedList' a -> SortedList' a / [ len xs , len ys ] @-}
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys   = ys
+merge xs []   = xs
+merge (x : xs) (y : ys)
+  | x <= y    = x : merge xs (y : ys)
+  | otherwise = y : merge (x : xs) ys
+
+{-@ msort :: Ord a => [a] -> SortedList' a @-}
+msort :: Ord a => [a] -> [a]
+msort []  = []
+msort [x] = [x]
+msort xs  =
+  case split xs of
+  -- case splitAt (length xs `div` 2) xs of
+    (ys, zs) -> merge (msort ys) (msort zs)
+
+{-@ split :: xs : [a] ->
+       { r : ([a], [a]) | (len (fst r) + len (snd r) == len xs)
+                      && ((len xs >= 2) => (len (fst r) > 0) && (len (snd r) > 0)) } @-}
+split :: [a] -> ([a], [a])
+split []           = ([], [])
+split [x]          = ([x], [])
+split (a : b : rs) = (a : as, b : bs)
+  where
+    (as, bs) = split rs
+
+{-@ measure validEmail :: String -> Bool @-}
